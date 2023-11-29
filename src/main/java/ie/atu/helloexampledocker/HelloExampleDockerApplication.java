215d29c48db37586b11ff4f946b16a15201a16eb
G00394277 <G00394277@atu.ie>
2023-11-29 12:47:44 +0000

    Initial commit

---
+++ src/main/java/ie/atu/helloexampledocker/HelloExampleDockerApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.helloexampledocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloExampleDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloExampleDockerApplication.class, args);
+    }
+
+}
